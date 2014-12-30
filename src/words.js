e2f662bd1ba3b50c66dc416c7e9344de30a50975
Denis <nutiudenis@hotmail.com>
2014-12-30 11:48:53 +0200

    Uploading files
    I still have no idea.

---
+++ src/words.js
@@ -0,0 +1,7 @@
+var animale_eu = ["pisica", "caine", "lup", "vulpe", "urs", "caprioara", "vaca", "cal", "oaie", "sobolan", "soarece"];
+
+var tari_eu = ["romania", "moldova", "germania", "spania", "franta", "rusia","italia", "elvetia", "suedia", "norvegia"];
+
+var firme = ["google", "yahoo", "microsoft", "oracle", "amazon", "emag", "ebay", "apple", "siemens", "intel", "toyota", "dacia", "volvo"];
+
+var ldp = ["javascript", "html", "css", "java", "c", "c++", "ruby", "pascal", "lisp", "erlang", "sql", "python"];
